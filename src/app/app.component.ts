8a34da2faa402fc160de9137e737c75e52f14b2b
Dheerendra-Shar <dheerendrasharma394@gmail.com>
2022-11-30 10:56:33 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stock-analysis';
+  safeSrc: SafeResourceUrl;
+  eps:number|undefined
+
+  epsGrowth: any |undefined
+  pe: number|undefined
+  epssssssss: number|undefined
+  intrinsic: number|undefined
+  date: Date;
+  intialincome: any;
+  constructor(private sanitizer: DomSanitizer) {
+    this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/8q0PRKHUgZc");
+    this.date = new Date()
+  }
+
+  calulatEps(netincome:any,totalshare:any){
+
+
+    this.epssssssss = netincome/totalshare;
+
+
+  }
+
+  calulatEpsgrowth(currentEPS:any,previousEPS:any,time:any){
+
+    this.epsGrowth = (Math.pow(currentEPS/previousEPS,1/time) -1 )*100 +"%"
+
+  }
+
+  calulatePe(marketvalue:any,eps:any){
+
+    this.pe= marketvalue/eps
+
+  }
+
+  calulateIe(eps:any,growthrate:any,peratio:any){
+    this.intrinsic = eps *(parseInt(growthrate)+1)*peratio
+
+  }
+
+  calulateIncome(cagr:any,timevalue:any,netincome:any){
+   let value =  (Math.pow(parseFloat(cagr)+1,timevalue))+ ''
+
+    this.intialincome = parseFloat(value)*netincome
+  }
 }
